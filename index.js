42807f8f48f5a2df6028a1c1a4bbef7dfabe2f94
Arzan Arif Pathan <arzan.pathan8@gmail.com>
2023-08-15 23:08:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
+let stopSaving = document.getElementById("save-btn");
 let count = 0;
+let noSaveElements = 0;
 let countEntries;
 function increment() {
     count += 1
@@ -10,19 +12,31 @@ function increment() {
 function save() {
     if(saveEl.textContent == "Previous Entries: "){
         countEntries = count;
-        console.log(saveEl.textContent);
     }
     else{
-        countEntries = count + " - ";
+        countEntries = " - " + count;
     }
+
     
+    noSaveElements += 1;
     saveEl.textContent += countEntries;
     countEl.textContent = 0;
-    count = 0;
+    count = 0;   
+    console.log(saveEl.textContent,) 
+    if(noSaveElements == 4){
+        console.log(noSaveElements, "value");        
+        stopSaving.disabled = true;
+        saveEl.insertAdjacentHTML("afterend", `<p class="red-col">Maximum 4 values can be saved</p>`);
+    }
+
+    
 }
 function clearscore() {
     saveEl.textContent = "Previous Entries: ";
     countEntries = ""; 
     countEl.textContent = 0;
     count = 0;
+    noSaveElements = 0;
+    stopSaving.disabled = false;
+    document.querySelector(".red-col").remove();
 }
